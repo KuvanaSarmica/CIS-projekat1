c8eee1d26b15e1bc054faaf2824c71f9937d9e68
KuvanaSarmica <miladinovmarkozr@gmail.com>
2026-07-29 22:41:13 +0200

    novi commit

--- blob_service.py
+++ blob_service.py
@@ -1,18 +1,26 @@
-import os, uuid
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import os
+from azure.storage.blob import BlobServiceClient
+import cv2
+import datetime as dt
+
+from dotenv import load_dotenv
+
+
 def save_snapshot(frame):
     try:
+        load_dotenv()
         print("Azure blob storage qucikstart")
         connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
     # Create the BlobServiceClient object
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-        container_name = 'mvm_screenshots'
-        container_client = blob_service_client.get_container_client()
-        blob_service_client.upload_blob(data)
-        
-
+        _, screenshot = cv2.imencode('.jpg',frame)
+        screenshot = screenshot.tobytes()
+        blob_client = blob_service_client.get_blob_client(
+            container=os.getenv('AZURE_CONTAINER_NAME'),
+            blob = dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jpg"
+        )
+        blob_client.upload_blob(screenshot)
     except Exception as ex:
         print('Exception:')
         print(ex)
--- motion_detector.py
+++ motion_detector.py
@@ -1,32 +1,20 @@
-import numpy as np
 import cv2
-from mypy.typeops import false_only
+from blob_service import save_snapshot
+from queue_service import send_motion_events
+import datetime as dt
 url = "http://192.168.1.14:4747/video"
 
-cap = cv2.VideoCapture(url)
+cap = cv2.VideoCapture()
 ret,frame1 = cap.read()
 ret,frame2 = cap.read()
-face_detected = False
-
-face_cascade = cv2.CascadeClassifier(
-    "haarcascade_frontalface_default.xml"
-)
-
-eye_cascade = cv2.CascadeClassifier(
-    "haarcascade_eye.xml"
-)
 
 
+
+
+poslednje_slanje=None
 while cap.isOpened():
 
     diff = cv2.absdiff(frame1, frame2)
-    gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 2)
-    for (x, y, w, h) in faces:
-        face_detected = True
-        diff[y:y + h + 20, x:x + w + 20] = 0
-        cv2.putText(frame1, f"Detektovano lice!", (400, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (0, 255, 0), 3)
 
     gray2 = cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray2,(5,5),0)
@@ -36,21 +24,23 @@ while cap.isOpened():
 
     mvm = False
 
-    for contour in contours:
-        (x,y,w,h) = cv2.boundingRect(contour)
 
+
+    for contour in contours:
         if cv2.contourArea(contour)<700:
             continue
         mvm = True
+
+
+
         (cx, cy, cw, ch) = cv2.boundingRect(contour)
         cv2.putText(frame1,f"Detektovan pokret!",(10,20),cv2.FONT_HERSHEY_SIMPLEX,
                     1,(0,0,255),3)
-
-
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame1, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
+    if mvm:
+        if poslednje_slanje == None or (dt.datetime.now() - poslednje_slanje).total_seconds()>10:
+            save_snapshot(frame1)
+            send_motion_events()
+            poslednje_slanje = dt.datetime.now()
     cv2.imshow('prozor',frame1)
 
     frame1=frame2
